1fe219cc355385e3c187b0d0bdce4b0b2cf913f9
Daisuke Fujita <dtanshi45@gmail.com>
2018-03-12 16:16:19 +0900

    Add /version endpoint

--- main.go
+++ main.go
@@ -10,11 +10,22 @@ const (
 	port = 8080
 )
 
+var (
+	version  string
+	revision string
+)
+
 func main() {
 	m := http.NewServeMux()
+
 	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello")
 		log.Println("/")
+	})
+
+	m.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Version: %s, Revision: %s\n", version, revision)
+		log.Println("/version")
 	})
 
 	log.Printf("server stated at localhost:%d", port)
